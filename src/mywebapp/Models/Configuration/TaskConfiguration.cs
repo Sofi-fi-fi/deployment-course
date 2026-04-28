5ca8a9877e6340befbede2ca76963c5bc24cd265
Sofi <lebedeffasofi@gmail.com>
2026-04-29 00:53:37 +0300

    fix: map entity properties to lowercase database columns

--- src/mywebapp/Models/Configuration/TaskConfiguration.cs
+++ src/mywebapp/Models/Configuration/TaskConfiguration.cs
@@ -12,15 +12,21 @@ public class TaskConfiguration : IEntityTypeConfiguration<TaskEntity>
 
                 builder.HasKey(t => t.Id);
 
+                builder.Property(t => t.Id)
+                        .HasColumnName("id");
+
                 builder.Property(t => t.Title)
+                        .HasColumnName("title")
                         .IsRequired()
                         .HasMaxLength(200);
 
                 builder.Property(t => t.Status)
+                        .HasColumnName("status")
                         .IsRequired()
                         .HasMaxLength(50);
 
                 builder.Property(t => t.Created_At)
+                        .HasColumnName("created_at")
                         .IsRequired();
         }
 }
